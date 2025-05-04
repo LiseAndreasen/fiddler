a8e1ac019419403b01d5b9e89ec4d42e860d0ce5
LiseAndreasen <lise.andreasen.1967@gmail.com>
2025-05-04 14:30:37 +0200

    #thisweeksfiddler 20250502

--- rides1.php
+++ rides1.php
@@ -1,1 +1,122 @@
-placeholder
+<?php
+
+// constants
+
+// fiddler or extra credit?
+$fiddler = 0;
+
+// number of times through loop
+$loops = 100000;
+
+// functions
+
+// choose nth zero value in array
+function choose_this($n, $arr) {
+	// so far none chosen
+	$chosen = 0;
+	foreach($arr as $key => $elem) {
+		if($elem == 0) {
+			$chosen++;
+		}
+		if($chosen == $n) {
+			return $key;
+		}
+	}
+}
+
+// find latest booked slot
+function latest_booked($arr) {
+	for($i=11;$i>=2;$i--) {
+		if($arr[$i] == 1) {
+			return $i;
+		}
+	}
+}
+
+function still_left($slots, $time) {
+	$sum = 0;
+	for($i=$time;$i<12;$i++) {
+		if($slots[$i] == 0) {
+			$sum++;
+		}
+	}
+	return $sum;
+}
+
+//////////////////////////////////////////////
+
+// sum of results
+$sum = 0;
+
+// loop of experiments
+for($j=0;$j<$loops;$j++) {
+	// there are 12 time slots
+	// at the beginning none of them are booked
+	for($i=0;$i<12;$i++) {
+		$slots[$i] = 0;
+	}
+
+	// then 3 are booked, at any time, not conflicting
+	for($i=0;$i<3;$i++) {
+		// how many are still available?
+		$left = 12 - array_sum($slots);
+		// choose one of these
+		$chosen = rand(1, $left);
+		$chosen_empty = choose_this($chosen, $slots);
+		$slots[$chosen_empty] = 1;
+	}
+
+	// keep riding and choosing a next, until the day is over
+	for($i=0;$i<12;$i++) {
+		if($slots[$i] == 1) {
+			// this slot is booked
+			// time to book a new slot
+			if($fiddler == 1) {
+				// next booked slot must be later than latest already booked
+				
+				// when is the latest booked slot?
+				$latest = latest_booked($slots);
+				
+				// we might be done
+				if($latest == 11) {
+					break;
+				}
+
+				// book!
+				// choose one of those after the latest
+				$chosen = rand($latest + 1, 11);
+				$slots[$chosen] = 1;
+			} else {
+				// next booked slot must be later than now
+				$now = $i;
+				// we might be done
+				if($now == 11) {
+					break;
+				}
+
+				// book!
+				// choose one of those after now
+				
+				$all_left = still_left($slots, 0);
+				$left = still_left($slots, $i);
+				if($left == 0) {
+					// we're done
+					break;
+				} else {
+					// choose an empty slot after now
+					$chosen = rand(1, $left) + $all_left - $left;
+					$chosen_empty = choose_this($chosen, $slots);
+					
+					$slots[$chosen_empty] = 1;
+				}
+			}
+		} else {
+		}
+	}
+
+	$sum += array_sum($slots);
+}
+
+printf("Expected number of rides: %1.4f\n", $sum / $loops);
+
+?>
--- rides2.php
+++ rides2.php
@@ -1,1 +1,123 @@
-placeholder
+<?php
+
+// constants
+
+// fiddler or extra credit?
+$fiddler = 0;
+
+// should a slot be added after the latest
+// or just not conflicting?
+$not_conflicting = 0;
+$after_latest = 1;
+
+// beginning situation
+$mode = $not_conflicting;
+
+// beginning time
+// later: the time for a slot being used
+$time = 0;
+
+// latest booked at the beginning
+$latest = -1;
+
+// functions
+
+function reset_slots() {
+	global $slots;
+	for($i=0;$i<12;$i++) {
+		$slots[$i] = 0;
+		$slots_order[$i] = ".";
+	}
+}
+
+function still_left($slots, $time) {
+	$sum = 0;
+	for($i=$time;$i<12;$i++) {
+		if($slots[$i] == 0) {
+			$sum++;
+		}
+	}
+	return $sum;
+}
+
+// add an extra slot to the program, trying all options
+function add_slots($slots, $mode, $time, $latest) {
+	// we arrive here, because the slot at time $time is being used!
+	
+	global $sum, $after_latest, $fiddler;
+
+	// how many are still available?
+	$all_left = still_left($slots, 0);
+	$left = still_left($slots, $time);
+
+	// nothing was booked for this time slot, move on
+	if($all_left <= 9 && $slots[$time] == 0) {
+		return add_slots($slots, $mode, $time + 1, $latest);
+	}
+	
+	// end case: the day is over, planning wise
+	if($time == 12 || $left == 0 || ($mode == $after_latest && $latest == 11)) {
+		return array_sum($slots);
+	}
+	
+	// may have to change mode
+	if($fiddler == 1 && $all_left == 9) {
+		$mode = $after_latest;
+	}
+	
+	// time only advances after 3 slots are booked
+	if($all_left <= 9) {
+		$thisTime = $time + 1;
+	} else {
+		$thisTime = $time;
+	}
+
+	// keep track of possible no of rides
+	$local_sum = 0;
+	// and number of tries
+	$local_tries = 0;
+	
+	for($i=$time;$i<12;$i++) {
+		if($mode == $after_latest && $i <= $latest) {
+			// can't book something before the latest booking
+			continue;
+		}
+		if($slots[$i] == 0) {
+			// this slot is available, try!
+			$slots[$i] = 1;
+			$slots_order[$i] = 12 - $all_left + 1;
+			if($latest < $i) {
+				$this_latest = $i;
+			} else {
+				$this_latest = $latest;
+			}
+			$local_tries++;
+			$local_sum += add_slots($slots, $mode, $thisTime, $this_latest);
+			$slots[$i] = 0;			
+			$slots_order[$i] = ".";
+		}
+	}
+	
+	if($local_tries == 0) {
+		// couldn't add another ride
+		return array_sum($slots);
+	} else {
+		return $local_sum / $local_tries;
+	}
+}
+
+//////////////////////////////////////////////
+
+// sum of results
+$sum = 0;
+
+// there are 12 time slots
+// at the beginning none of them are booked
+reset_slots();
+
+// go recursively through all options
+$expected = add_slots($slots, $mode, $time, $latest);
+
+printf("Expected number of rides: %1.4f\n", $expected);
+
+?>
