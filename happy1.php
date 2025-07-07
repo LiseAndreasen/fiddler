4be53a1566be69e9e67367b74b64597b1774514c
LiseAndreasen <lise.andreasen.1967@gmail.com>
2025-07-07 20:03:50 +0200

    #thisweeksfiddler 20250704

--- happy1.php
+++ happy1.php
@@ -1,1 +1,191 @@
-placeholder
+<?php
+
+// constants
+
+// size of dozo board
+$dozo = 7;
+
+// i goes down, j goes to the right
+// the board is squished together and does not preserve 60 deg angles
+
+// board
+// include a lot of the space around the board
+for($i=-$dozo;$i<=$dozo*2;$i++) {
+	for($j=-$dozo;$j<=$dozo*3;$j++) {
+		if(($i + $j) % 2 == 0) {
+			// so far empty space
+			$board[$i][$j] = 0;
+		} else {
+			// there couldn't even be a hole here
+			$board[$i][$j] = " ";
+		}
+	}
+}
+
+// then mark the holes
+for($i=1;$i<=$dozo;$i++) {
+	for($j=1;$j<=$dozo*2-1;$j++) {
+		if(($i + $j) % 2 == 0) {
+			if($i <= $j && $j <= $dozo * 2 - $i) {
+				// a hole!
+				$board[$i][$j] = 1;
+				$holes[] = array($i, $j);
+			}
+		}
+	}
+}
+
+// functions
+
+function print_board() {
+	global $dozo, $board;
+	for($i=-$dozo;$i<=$dozo*2;$i++) {
+		for($j=-$dozo;$j<=$dozo*3;$j++) {
+			print $board[$i][$j];
+		}
+		print "\n";
+	}	
+}
+
+function print_triangles() {
+	global $holes, $triangles;
+	$triangle_no = 0;
+	foreach($triangles as $t1 => $triangle1) {
+		foreach($triangle1 as $t2 => $triangle2) {
+			foreach($triangle2 as $t3 => $triangle3) {
+				$h1i = $holes[$t1][0];
+				$h1j = $holes[$t1][1];
+				$h2i = $holes[$t2][0];
+				$h2j = $holes[$t2][1];
+				$h3i = $holes[$t3][0];
+				$h3j = $holes[$t3][1];
+				//printf("Triangle: (%d,%d), (%d,%d), (%d,%d).\n", $h1i, $h1j, $h2i, $h2j, $h3i, $h3j);
+				$triangle_no++;
+			}
+		}
+	}
+	print("There were $triangle_no triangles in all.\n");
+}
+
+function add_triangle($key1, $key2, $i3, $j3) {
+	global $holes, $triangles;
+	$hole3 = array($i3, $j3);
+	$key3 = array_search($hole3, $holes);
+	$triangle = array($key1, $key2, $key3);
+	sort($triangle);
+	$triangles[$triangle[0]][$triangle[1]][$triangle[2]] = 1;
+}
+
+function test_cases($a, $b, $key1, $i1, $j1, $key2, $i2, $j2) {
+	global $board;
+	if($i1 + $b == $i2 && $j1 + 2 * $a + $b == $j2) {
+		$i3 = $i1 + $a + $b;
+		$j3 = $j1 + $a - $b;
+		if($board[$i3][$j3] == 1) {
+			add_triangle($key1, $key2, $i3, $j3);
+		}
+		
+		$i3 = $i2 - $a - $b;
+		$j3 = $j2 - $a + $b;
+		if($board[$i3][$j3] == 1) {
+			add_triangle($key1, $key2, $i3, $j3);
+		}
+	}
+	
+	if($i1 + $b == $i2 && $j2 + 2 * $a + $b == $j1) {
+		$i3 = $i1 + $a + $b;
+		$j3 = $j1 - $a + $b;
+		if($board[$i3][$j3] == 1) {
+			add_triangle($key1, $key2, $i3, $j3);
+		}
+
+		$i3 = $i1 - $a - $b;
+		$j3 = $j1 + $a - $b;
+		if($board[$i3][$j3] == 1) {
+			add_triangle($key1, $key2, $i3, $j3);
+		}
+	}
+}
+
+//////////////////////////////////////////////
+
+foreach($holes as $key1 => $hole1) {
+	foreach($holes as $key2 => $hole2) {
+		if($key2 <= $key1) {
+			// skip duplicates
+			continue;
+		}
+		$i1 = $hole1[0];
+		$j1 = $hole1[1];
+		$i2 = $hole2[0];
+		$j2 = $hole2[1];
+		
+		if($i1 == $i2) {
+			// a line in the triangle is parallel with the side of the board
+			// the 3rd hole must be above or below
+			$jdist = $j2 - $j1;
+			
+			// check above
+			$i3 = $i1 - $jdist / 2;
+			$j3 = $j1 + $jdist / 2;
+			if($board[$i3][$j3] == 1) {
+				add_triangle($key1, $key2, $i3, $j3);
+			}
+			
+			// check below
+			$i3 = $i1 + $jdist / 2;
+			$j3 = $j1 + $jdist / 2;
+			if($board[$i3][$j3] == 1) {
+				add_triangle($key1, $key2, $i3, $j3);
+			}
+		} else {
+			if($j1 == $j2) {
+				// a line in the triangle is perpendicular to side of the board
+				// the 3rd hole will be to the left or right
+				$idist = $i2 - $i1;
+				
+				// check left
+				$i3 = $i1 + $idist / 2;
+				$j3 = $j1 - $idist * 1.5;
+				if($board[$i3][$j3] == 1) {
+					add_triangle($key1, $key2, $i3, $j3);
+				}
+
+				// check right
+				$i3 = $i1 + $idist / 2;
+				$j3 = $j1 + $idist * 1.5;
+				if($board[$i3][$j3] == 1) {
+					add_triangle($key1, $key2, $i3, $j3);
+				}
+			} else {
+				// now for the weird cases
+				
+				// width = distance between 2 holes
+				// 1 side is move a widths to the right,
+				// make a 30 deg turn to the right,
+				// move b further widths forwards
+				// or the opposite: a left, turn left, b forward
+				
+				$a = 2;
+				$b = 1;
+				test_cases($a, $b, $key1, $i1, $j1, $key2, $i2, $j2);
+
+				$a = 3;
+				$b = 1;
+				test_cases($a, $b, $key1, $i1, $j1, $key2, $i2, $j2);
+
+				$a = 4;
+				$b = 1;
+				test_cases($a, $b, $key1, $i1, $j1, $key2, $i2, $j2);
+				
+				// a = 5 is too far
+				// a = 3, b = 2, nothing new
+				// a = 4, b = 3, nothing new
+			}
+		}
+	}
+}
+
+print_triangles();
+
+?>
--- happy2.php
+++ happy2.php
@@ -1,1 +1,147 @@
-placeholder
+<?php
+
+// constants
+
+// size of flag
+$flag1 = 5;
+$flag2 = 6;
+
+// i goes down, j goes to the right
+
+// board
+// include a lot of the space around the board
+for($i=-$flag1*2;$i<=$flag1*4;$i++) {
+	for($j=-$flag2*2;$j<=$flag2*4;$j++) {
+		if(($i + $j) % 2 == 0) {
+			// so far empty space
+			$board[$i][$j] = 0;
+		} else {
+			// there couldn't even be a hole here
+			$board[$i][$j] = " ";
+		}
+	}
+}
+
+// then mark the holes
+for($i=1;$i<=$flag1*2-1;$i++) {
+	for($j=1;$j<=$flag2*2-1;$j++) {
+		if(($i + $j) % 2 == 0) {
+				// a hole!
+				$board[$i][$j] = 1;
+				$holes[] = array($i, $j);
+		}
+	}
+}
+
+// functions
+
+function print_board() {
+	global $flag1, $flag2, $board;
+	for($i=-$flag1*2;$i<=$flag1*4;$i++) {
+		for($j=-$flag2*2;$j<=$flag2*4;$j++) {
+			print $board[$i][$j];
+		}
+		print "\n";
+	}	
+}
+
+function print_parallelograms() {
+	global $holes, $parallelograms;
+	$parallelogram_no = 0;
+	foreach($parallelograms as $p1 => $parallelogram1) {
+		foreach($parallelogram1 as $p2 => $parallelogram2) {
+			foreach($parallelogram2 as $p3 => $parallelogram3) {
+				foreach($parallelogram3 as $p4 => $parallelogram4) {
+					$h1i = $holes[$p1][0];
+					$h1j = $holes[$p1][1];
+					$h2i = $holes[$p2][0];
+					$h2j = $holes[$p2][1];
+					$h3i = $holes[$p3][0];
+					$h3j = $holes[$p3][1];
+					$h4i = $holes[$p4][0];
+					$h4j = $holes[$p4][1];
+					//printf("Parallelogram: (%d,%d), (%d,%d), (%d,%d), (%d,%d).\n", $h1i, $h1j, $h2i, $h2j, $h3i, $h3j, $h4i, $h4j);
+					$parallelogram_no++;
+				}
+			}
+		}
+	}
+	print("There were $parallelogram_no parallelograms in all.\n");
+}
+
+function add_parallelogram($key1, $key2, $key3, $i4, $j4) {
+	global $holes, $parallelograms;
+	$hole4 = array($i4, $j4);
+	$key4 = array_search($hole4, $holes);
+	$parallelogram = array($key1, $key2, $key3, $key4);
+	sort($parallelogram);
+	$parallelograms[$parallelogram[0]][$parallelogram[1]][$parallelogram[2]][$parallelogram[3]] = 1;
+}
+
+//////////////////////////////////////////////
+
+foreach($holes as $key1 => $hole1) {
+	foreach($holes as $key2 => $hole2) {
+		if($key1 == $key2) {
+			// skip duplicates
+			continue;
+		}
+		foreach($holes as $key3 => $hole3) {
+			if($key1 == $key3 || $key2 == $key3) {
+				continue;
+			}
+			$i1 = $hole1[0];
+			$j1 = $hole1[1];
+			$i2 = $hole2[0];
+			$j2 = $hole2[1];
+			$i3 = $hole3[0];
+			$j3 = $hole3[1];
+			
+			// exclude 3 points on a line
+			if($j3 == $j1) {
+				$slopea = 1000;
+			} else {
+				$slopea = ($i3 - $i1) / ($j3 - $j1);
+			}
+			if($j3 == $j2) {
+				$slopeb = 1000;
+			} else {
+				$slopeb = ($i3 - $i2) / ($j3 - $j2);
+			}
+			if($j2 == $j1) {
+				$slopec = 1000;
+			} else {
+				$slopec = ($i2 - $i1) / ($j2 - $j1);
+			}
+			if($slopea == $slopeb && $slopea == $slopec) {
+				continue;
+			}
+			
+			// parallelogram:
+			// add 4th hole to existing triangle
+			// add coordinates for 2 holes
+			// subtract 3rd
+			$i4 = $i1 + $i2 - $i3;
+			$j4 = $j1 + $j2 - $j3;
+			if($board[$i4][$j4] == 1) {
+				add_parallelogram($key1, $key2, $key3, $i4, $j4);
+			}
+
+			$i4 = $i1 - $i2 + $i3;
+			$j4 = $j1 - $j2 + $j3;
+			if($board[$i4][$j4] == 1) {
+				add_parallelogram($key1, $key2, $key3, $i4, $j4);
+			}
+
+			$i4 = - $i1 + $i2 + $i3;
+			$j4 = - $j1 + $j2 + $j3;
+			if($board[$i4][$j4] == 1) {
+				add_parallelogram($key1, $key2, $key3, $i4, $j4);
+			}
+		}
+	}
+}
+
+print_parallelograms();
+
+?>
