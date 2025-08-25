0ab865eea587d4e1ea7ae9cd2438cd6677176107
LiseAndreasen <lise.andreasen.1967@gmail.com>
2025-08-25 02:13:36 +0200

    #thisweeksfiddler 20250822

--- sundown1.php
+++ sundown1.php
@@ -1,2 +1,131 @@
-placeholder
-should be updated within 24 hours
+<?php
+
+///////////////////////////////////////////////////////////////////////////
+// constants
+
+// the time left
+// 5.55 - 7.00
+// 65 minutes
+$time_left = 65;
+
+// 4 different sizes of loops, measured in minutes!
+$loops = array(45, 35, 30, 10);
+$no_of_loops = sizeof($loops);
+
+///////////////////////////////////////////////////////////////////////////
+// functions
+
+function loop_on(& $averages, $that_time_left, $mulligan_left) {
+	global $no_of_loops, $loops, $mulligans;
+	if(isset($averages[$that_time_left][$mulligan_left])) {
+		return $averages[$that_time_left][$mulligan_left];
+	}
+	
+	// use recursion
+	for($i=0;$i<$no_of_loops;$i++) {
+		$this_loop_time = $loops[$i];
+		$this_time_left = $that_time_left - $this_loop_time;
+		// may use mulligan
+		if(isset($mulligans[$that_time_left][$this_loop_time])
+			&& strcmp($mulligans[$that_time_left][$this_loop_time], "mull") == 0
+			&& $mulligan_left == 1) {
+			$tmp_score = loop_on($averages, $that_time_left, 0);
+		} else {
+			if($this_time_left < 0) {
+				$tmp_score = 0;
+			} else {
+				$tmp_score = $this_loop_time
+					+ loop_on($averages, $this_time_left, $mulligan_left);
+			}
+		}
+		$loop_score[] = $tmp_score;
+	}
+
+	$average = array_sum($loop_score)/count($loop_score);
+	$averages[$that_time_left][$mulligan_left] = $average;
+	return $average;
+}
+
+function print_2d($arr) {
+	print("  ");
+	foreach($arr[0] as $arrcol => $arrval) {
+		printf("%4d ", $arrcol);
+	}
+	echo "\n";
+	foreach($arr as $arrj => $arrw) {
+		printf("%2d ", $arrj);
+		foreach($arrw as $arri => $arrc) {
+			print($arr[$arrj][$arri] . " ");
+		}
+		echo "\n";
+	}
+}
+
+function print_3d($arr) {
+	print("  ");
+	foreach($arr[0] as $arrcol => $arrval) {
+		printf("%7d ", $arrcol);
+	}
+	echo "\n";
+	foreach($arr as $arrj => $arrw) {
+		printf("%2d ", $arrj);
+		printf("%7.4f ", $arr[$arrj][0]);
+		if(isset($arr[$arrj][1])) {
+			printf("%7.4f ", $arr[$arrj][1]);
+		}
+		echo "\n";
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////
+// main program
+
+// averages already calculated
+$averages = array();
+$averages[0][0] = 0;
+$averages[0][1] = 0;
+
+// calculate averages without the mulligan
+loop_on($averages, $time_left, 0);
+
+printf("Miles added to score on average: %.3f\n\n", $averages[$time_left][0] / 10);
+
+print("Mulligan added.\n\n");
+
+// calculate the mulligan table
+foreach($loops as $loop_time) {
+	foreach($averages as $avg_time_left => $average_arr) {
+		$average = $average_arr[0];
+		if($avg_time_left == $loop_time) {
+			$mulligans[$avg_time_left][$loop_time] = "keep";
+			continue;
+		}
+		if($avg_time_left < $loop_time) {
+			$mulligans[$avg_time_left][$loop_time] = "mull";
+			continue;
+		}
+		$new_time_left = $avg_time_left - $loop_time;
+		$avg_score_keep = $averages[$new_time_left][0] + $loop_time;
+		$avg_score_mull = $average;
+		if($avg_score_keep < $avg_score_mull) {
+			$mulligans[$avg_time_left][$loop_time] = "mull";
+		} else {
+			$mulligans[$avg_time_left][$loop_time] = "keep";
+		}
+	}
+}
+
+print("Mulligan table\n\n");
+print_2d($mulligans);
+print("\n");
+
+// calculate averages with the mulligan
+loop_on($averages, $time_left, 1);
+
+print("Averages\n\n");
+print_3d($averages);
+print("\n");
+
+printf("Miles added to score on average, with mulligan: %.3f\n\n", $averages[$time_left][1] / 10);
+
+?>
--- sundown2.php
+++ sundown2.php
@@ -1,2 +1,67 @@
-placeholder
-should be updated within 24 hours
+<?php
+
+///////////////////////////////////////////////////////////////////////////
+// constants
+
+// the time left
+// 5.55 - 7.00
+// 65 minutes
+$time_left = 65;
+
+// 4 different sizes of loops, measured in minutes!
+$loops = array(45, 35, 30, 10);
+$no_of_loops = sizeof($loops);
+
+$runs = 1000000;
+
+$mulligans = array(
+	00 => array("mull", "mull", "mull", "mull"),
+	05 => array("mull", "mull", "mull", "mull"),
+	10 => array("mull", "mull", "mull", "keep"),
+	15 => array("mull", "mull", "mull", "keep"),
+	20 => array("mull", "mull", "mull", "keep"),
+	25 => array("mull", "mull", "mull", "keep"),
+	30 => array("mull", "mull", "keep", "keep"),
+	35 => array("mull", "keep", "keep", "mull"),
+	45 => array("keep", "keep", "mull", "mull"),
+	55 => array("keep", "mull", "mull", "keep"),
+	65 => array("mull", "mull", "keep", "keep")
+);
+
+///////////////////////////////////////////////////////////////////////////
+// functions
+
+///////////////////////////////////////////////////////////////////////////
+// main program
+
+for($mulligan_left=0;$mulligan_left<=10;$mulligan_left++) {
+	$score_sum = 0;
+	for($i=0;$i<$runs;$i++) {
+		$this_mulligan = $mulligan_left;
+		$score = 0;
+		$this_time_left = $time_left;
+		while($this_time_left > 0) {
+			$loop_chosen = rand(0, 3);
+			$loop_time = $loops[$loop_chosen];
+			if($this_mulligan > 0
+				&& strcmp($mulligans[$this_time_left][$loop_chosen], "mull") == 0)
+			{
+				$this_mulligan--;
+			} else {
+				if($this_time_left < $loop_time) {
+					// run over
+					$this_time_left -= $loop_time;
+				} else {
+					$this_time_left -= $loop_time;
+					$score += $loop_time;
+				}
+			}
+		}
+		$score_sum += $score;
+	}
+
+	printf("Expected score with %2d mulligans: %.3f\n",
+		$mulligan_left, $score_sum / $runs);
+}
+
+?>
