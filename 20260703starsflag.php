bc65a07b8937810e0dc9d7d93c51d33113a81034
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-07-05 10:15:32 +0200

    small improvements

--- 20260703starsflag.php
+++ 20260703starsflag.php
@@ -32,43 +32,53 @@ function stars_max($d1, $r) {
 	// position of center of circle
 	// vary x and y, 0 - 0.5
 	for($x1=0;$x1<=0.5;$x1+=$d1) {
-		for($y1=0;$y1<=0.5;$y1+=$d1) {
+		for($y1=$x1;$y1<=0.5;$y1+=$d1) {
 			// radius of circle
 			$stars = count_stars($x1, $y1, $r);
 			if($stars_max < $stars) {
 				$stars_max = $stars;
+				$xy = sprintf("x1: %.6f, y1: %.6f", $x1, $y1);
 			}
 		}
 	}
-	return $stars_max;
+	return [$stars_max, $xy];
 }
 
 ///////////////////////////////////////////////////////////////////////////
 // main program
 
 // delta
-$d1 = 0.001;		// fiddler, 0.001 is good
-$stars_max = stars_max($d1, 2);
+$d1 = 0.0001;
+[$stars_max, $xy] = stars_max($d1, 2);
 
-printf("Result 1: %d\n\n", $stars_max);
+printf("Result 1: %d (%s)\n\n", $stars_max, $xy);
 
 ///////////////////////////////////////////////////////////////////////////
 
-$r_min = 4.14;
-$r_max = 4.18;
+// guesses at r will be guided by info from Desmos, where r <= 4.155193
 
-$d1 = 0.0005;
+$d = 0.0001;
+for($r=4.15518;$r<=4.15521;$r+=0.00001) {
+	[$stars, $xy] = stars_max($d, $r);
+	printf("Max stars for r = %.6f, d = %.6f: %2d (%s)\n", $r, $d, $stars, $xy);
+}
+
+$r_min = 4.155;
+$r_max = 4.156;
+
+$d1 = 0.0001;
 
 while(0.00001 < $r_max - $r_min) {
 	printf("%.7f < r < %.7f\n", $r_min, $r_max);
 	$r_mid = ($r_min + $r_max) / 2;
-	$stars_r_mid = stars_max($d1, $r_mid);
+	[$stars_r_mid, $xy] = stars_max($d1, $r_mid);
 	if($stars_r_mid < 58) {
 		$r_min = $r_mid;
 	} else {
 		$r_max = $r_mid;
+		$x_max = $xy;
 	}
 }
-printf("\nResult 2: %.7f - %.7f\n", $r_min, $r_max);
+printf("\nResult 2: %.7f - %.7f (%s)\n", $r_min, $r_max, $x_max);
 
 ?>
