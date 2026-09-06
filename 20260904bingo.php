0827c34c6f7fee59c84eb1c258cbe858b1f9c5e7
LiseAndreasen <lise.andreasen.1967@gmail.com>
2026-09-06 09:04:09 +0200

    added histogram

--- 20260904bingo.php
+++ 20260904bingo.php
@@ -9,10 +9,45 @@ $small_card_size = 5;
 $large_card_size = 8;
 $bingo_size = 5;
 $bingo_marked = 0;
-$loops = 1000000;
+$loops = 100000;
 
 ///////////////////////////////////////////////////////////////////////////
 // functions
+
+function print_histogram($data) {
+    $no_of_buckets = sizeof($data);
+    $max_bucket = max($data) + 1;
+    // print histogram
+    // height
+    $h = 20;
+    for($i=1;$i<$h+1;$i++) {
+        $histogram[$i] = "";
+    }
+    $histogram[0] = "+";
+    // assuming no of buckets less than width of screen
+    for($i=1;$i<=1+$no_of_buckets/10;$i++) {
+        $histogram[0] .= "---------+";
+    }
+    $histogram[$h+1] = $histogram[0];
+    
+    $sum_of_all = 0;
+    $no_of_all = 0;
+    for($i=0;$i<$no_of_buckets;$i++) {
+        $column_top = (int) (($h + 1) * $data[$i] / $max_bucket);
+        for($j=1;$j<=$column_top;$j++) {
+            $histogram[$j] .= "X";
+        }
+        for($j=$column_top+1;$j<$h+1;$j++) {
+            $histogram[$j] .= " ";
+        }
+        $sum_of_all += $i * $data[$i];
+        $no_of_all += $data[$i];
+    }
+    for($i=$h+1;$i>=0;$i--) {
+        print($histogram[$i] . "\n");
+    }
+    printf("Average: %.5f\n\n", $sum_of_all / $no_of_all);
+}
 
 function create_bingocard($width) {
     global $max_no, $small_card_size, $large_card_size;
@@ -128,11 +163,24 @@ function check_bingo($card) {
 
 $small_bingos = 0;
 $large_bingos = 0;
+
+// data for histogram
+for($i=0;$i<=$large_card_size*$large_card_size;$i++) {
+    $small_bingo_at_alone[$i] = 0;
+    $large_bingo_at_alone[$i] = 0;
+    $small_bingo_at_combined[$i] = 0;
+    $large_bingo_at_combined[$i] = 0;
+}
+
 for($j=0;$j<$loops;$j++) {
     // progress
     if($j % 5000 == 0) {
         printf("%.3f ", $j / $loops);
     }
+    
+    // data for histogram
+    $small_bingo = 0;
+    $large_bingo = 0;
     
     [$small_card_matrix, $small_card_list] =
         create_bingocard($small_card_size);
@@ -153,7 +201,10 @@ for($j=0;$j<$loops;$j++) {
             [$r, $c] = $small_card_list[$called_number];
             $small_card_matrix[$r][$c] = $bingo_marked;
             $bingo = check_bingo($small_card_matrix);
+            $small_bingo++;
             if($bingo) {
+                $small_bingo_at_alone[$small_bingo]++;
+                $small_bingo_at_combined[$small_bingo+$large_bingo]++;
                 $small_bingos++;
                 $bingo_called = true;
                 break;
@@ -163,7 +214,10 @@ for($j=0;$j<$loops;$j++) {
             [$r, $c] = $large_card_list[$called_number];
             $large_card_matrix[$r][$c] = $bingo_marked;
             $bingo = check_bingo($large_card_matrix);
+            $large_bingo++;
             if($bingo) {
+                $large_bingo_at_alone[$large_bingo]++;
+                $large_bingo_at_combined[$small_bingo+$large_bingo]++;
                 $large_bingos++;
                 $bingo_called = true;
                 break;
@@ -178,5 +232,15 @@ print("\n\n");
 
 printf("Bingos on small vs large cards: %.5f %.5f\n",
     $small_bingos / $loops, $large_bingos / $loops);
+
+print("Distribution, how many numbers necessary for bingo.\n\n");
+print("Only looking at small card, igonoring numbers on large card.\n\n");
+print_histogram($small_bingo_at_alone);
+print("Same, for large card.\n\n");
+print_histogram($large_bingo_at_alone);
+print("Looking at small card bingos, including numbers on large.\n\n");
+print_histogram($small_bingo_at_combined);
+print("Same, for large card.\n\n");
+print_histogram($large_bingo_at_combined);
 
 ?>
